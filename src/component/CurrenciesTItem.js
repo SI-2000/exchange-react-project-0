6ff424f12d33f340b007c7fe9100e5ee0e971fc7
Siamak Irani <siamakirani1378@gmail.com>
2023-11-21 18:06:20 +0330

    refactor: Refactoring HomePage currenceis list

--- src/component/CurrenciesTItem.js
+++ src/component/CurrenciesTItem.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-
 import { Link } from "react-router-dom";
 import { roundTo } from "../util/round-number";
+import { ReactComponent as MoreBtn } from "../files/icons/more_vert_FILL0_wght400_GRAD0_opsz24.svg";
 import classes from "./CurrenciesTItem.module.css";
+import TradeOverly from "./TradeOverly";
 
 const CurrenciesTItem = ({ currencyInfo }) => {
-  console.log(currencyInfo);
+  const [overlyIsVisiable, setOverlyIsVisiable] = useState(false);
 
   const {
     id,
@@ -17,7 +18,7 @@ const CurrenciesTItem = ({ currencyInfo }) => {
   } = currencyInfo;
 
   return (
-    <tr>
+    <tr className={`${classes["currencies-item"]}`}>
       <td className={classes["name"]}>
         <img alt={currencyInfo.coinId} src={image} />
         <div className={classes["name-symbol"]}>
@@ -25,32 +26,33 @@ const CurrenciesTItem = ({ currencyInfo }) => {
           <p>{symbol.toUpperCase()}</p>
         </div>
       </td>
-      <td className={classes["price"]}>{roundTo(current_price, 4)} $</td>
+      <td className={classes["price"]}>{roundTo(current_price, 2)} $</td>
       <td
         className={`${classes["change"]} ${
           change >= 0 ? classes["increased"] : classes["decreased"]
         }`}
       >
         {change >= 0 && "+"}
-        {change}%
+        {roundTo(change, 2)}%
       </td>
       <td className={classes["volume"]}>{volume}</td>
       <td className={classes["buy-sell-btn"]}>
-        <td className={classes["buy-sell-btn"]}>
-          <button
-            onClick={() => {
-              setOverlyIsVisiable((prevState) => {
-                return !prevState;
-              });
-            }}
-            className={classes["more-btn"]}
-          >
-            <MoreBtn />
-          </button>
-          {overlyIsVisiable && (
-            <TradeOverly onClickOnBG={setOverlyIsVisiable} />
-          )}
-        </td>
+        <button
+          onClick={() => {
+            setOverlyIsVisiable((prevState) => {
+              return !prevState;
+            });
+          }}
+          className={classes["more-btn"]}
+        >
+          <MoreBtn />
+        </button>
+        {overlyIsVisiable && (
+          <TradeOverly
+            onClickOnBG={setOverlyIsVisiable}
+            className="currency-item--trade-overly"
+          />
+        )}
       </td>
     </tr>
   );
