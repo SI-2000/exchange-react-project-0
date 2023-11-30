3c019c1b1ce744ce734705a2baf26d99af612d36
Siamak Irani <siamakirani1378@gmail.com>
2023-11-30 23:01:40 +0330

    feat:Adding auth page images

--- src/pages/Authentication.js
+++ src/pages/Authentication.js
@@ -4,7 +4,6 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
-
 import AuthForm from "../component/AuthForm";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -18,10 +17,16 @@ import { authActions } from "../store/auth";
 import { firebaseErrHandler } from "../util/fb-auth-err-handler";
 import { fetchRealTimeDB } from "../util/realTimeDB-req";
 import { extractUsername } from "../util/username-extracter";
+import classes from "./Authentication.module.css";
+import AuthImage0 from "../files/images/login-image/auth-0.jfif";
+import AuthImage1 from "../files/images/login-image/auth-1.jfif";
 
 const Authentication = () => {
   const uid = useSelector((state) => state.auth.uid);
   const navigate = useNavigate();
+
+  const [searchParams] = useSearchParams();
+  const isLogin = searchParams.get("mode") === "login";
 
   useEffect(() => {
     if (uid) {
@@ -29,7 +34,14 @@ const Authentication = () => {
     }
   }, []);
 
-  return <AuthForm />;
+  return (
+    <div className={`${classes["Authentication"]}`}>
+      <AuthForm />
+      <div className={`${classes["image-container"]}`}>
+        {isLogin ? <img src={AuthImage0}></img> : <img src={AuthImage1}></img>}
+      </div>
+    </div>
+  );
 };
 
 export default Authentication;
@@ -55,7 +67,6 @@ export async function action({ request }) {
           authData.password
         );
         let user = userCredential.user;
-        console.log(user)
 
         const userData = {
           email: user.email,
