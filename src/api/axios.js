304c1522030c96a0434998f04d8030e0d06c3121
siamak irani <siamakirani1378@gmail.com>
2024-01-23 12:40:52 +0330

    Some cleaning

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,5 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000";
-
-export default axios.create({
-  baseURL: BASE_URL,
-});
-
 export const fireBaseAxios = axios.create({
   baseURL: "https://exchange-3f817-default-rtdb.firebaseio.com/",
 });
